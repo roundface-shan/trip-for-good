436f6895415e0bd16d00805f6773b16cb1dd5f98
roundface_shan <lice2567614@gmail.com>
2023-04-11 13:45:24 +0800

    Changes to be committed: 	modified:   src/pages/search/SearchPage.tsx
     Changes not staged for commit:
    	deleted:    src/components/filter/Filter.tsx
    	deleted:    src/components/filter/FilterArea.module.css
    	deleted:    src/components/filter/FilterArea.tsx
    	deleted:    src/components/filter/FilterTag.tsx
    	deleted:    src/components/filter/index.ts
    	deleted:    src/components/productList/ProductList.tsx
    	deleted:    src/components/productList/index.ts

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,6 +1,6 @@
 import styles from './SearchPage.module.css';
 import React from 'react';
-import { Header, Footer, FilterArea, ProductList } from '../../components';
+import { Header } from '../../components/Header';
 
 export const SearchPage: React.FC = () => {
     return (
@@ -9,11 +9,11 @@ export const SearchPage: React.FC = () => {
                 <div className={styles['page-content']}>
                 {/* 分类过滤器 */}
                     <div className={styles['product-list-container']} >
-                        <FilterArea />
+
                     </div>
                 {/* 产品列表 */}
                     <div className={styles['product-list-container']} >
-                        <ProductList />
+                        
                     </div>
                 </div>
             <Footer />
