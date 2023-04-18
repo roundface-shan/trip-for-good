7e6eabf1fe3f88566a080cb01aaf6b057ae09244
roundface_shan <lice2567614@gmail.com>
2023-04-18 12:49:31 +0800

    Changes to be committed: 	new file:   src/pages/register/RegisterForm.module.css 	modified:   src/pages/register/RegisterPage.tsx 	new file:   src/pages/register/RehisterForm.tsx

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./Register.module.css";
 import { UserLayout } from "../../layouts/userLayout";
+import { RegisterForm } from "./RehisterForm";
 
 export const RegisterPage: React.FC = () => {
     return (
         <UserLayout>
-            <div>Register</div>
+            <RegisterForm />
         </UserLayout>
     );
 };
